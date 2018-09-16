4b2b22533de88bf12abec62acdfb2d892a60b90d
Arjun Kumar <akmu999@aucklanduni.ac.nz>
2018-09-16 20:11:31 +1200

    form added for city input, had trouble adding the material ui form

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,14 @@
-import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 import * as React from 'react';
 
 import './App.css';
 
+
 interface IState {
-  imageFiles: any[],
+  userInput: any,
   results: any,
+  findTemperature: any,
 }
 
 export default class App extends React.Component<{}, IState>{
@@ -15,49 +17,60 @@ export default class App extends React.Component<{}, IState>{
   constructor(props: any) {
     super(props)
     this.state = {
-      imageFiles: [],
-      results: ""
+      userInput: "London",
+      results: this.weatherAPI(),
+      findTemperature: this.getUserInput(),
     }
   }
 
+  
+  public getUserInput(){
+    return null;
+  }
 
-
-  public upload(base64String: string) {
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=Berlin&units=metric&APPID=5dc0e44258249f73a827437cdb3b54bc', {
+  public weatherAPI() {
+    /*Method to receive data from weather api*/ 
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=5dc0e44258249f73a827437cdb3b54bc', {
       method: 'POST',
       headers: {
         'Content-Type': 'text/plain',
       },
-      body: JSON.stringify({
-        file: base64String,
-      })
     })
     .then((response : any) => {
       if (!response.ok) {
         this.setState({results: response.statusText})
       }
       else {
-        response.json().then((data:any) => this.setState({results: data.main.temp}))
+        response.json().then((data:any) => this.setState({results: data.main.temp})) /*response from api call */
       }
       return response
     })
   }
-  public render() {
-    return (
-      <div className="container-fluid">
-        <div className="centreText">
-   
-           
-     
-          <div  className="dank">
-          {
-            this.state.results === "" && this.state.imageFiles.length > 0 ?
-            <CircularProgress thickness={3} /> :
-            <p>{this.state.results}</p>
-          }
-          </div>
-        </div>
-      </div>
-    );
-  }
+
+  
+
+  
+    public render() {
+
+      return (
+
+
+          <div className="centreText">
+            <form onSubmit={this.weatherAPI}>
+                <label>
+                  Name:
+                  <input type="text" value={this.state.userInput} />
+                </label>
+                <input type="submit" value="Submit" />
+              </form>
+            {
+              <p>{this.state.results}</p>
+            }
+            </div>
+        
+          )
+    }
+    
+
+  
 }
--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,21 +1,24 @@
 
-import {AppBar, IconButton, Toolbar, Typography} from '@material-ui/core/';
-import MenuIcon from '@material-ui/icons/Menu';
+import {AppBar, Toolbar, Typography} from '@material-ui/core/';
+
+
 import * as React from 'react';
 
 import { Link } from 'react-router-dom';
 
 
-
 export const Header: React.StatelessComponent<{}> = () => {
+
+    
+
+    
     return (
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton  aria-label="Menu" color="inherit">
-                        <MenuIcon aria-haspopup="true"/>
-                    </IconButton>
+
                     <Typography variant="display2" color="inherit">
                         <Link style={{color: "white"}} to="/">Find City Temperature</Link>
+
                         <Link to="/FirstComponent"> London </Link>
                         <Link to="/SecondComponent"> Paris </Link>
                         < Link to= "/ThirdComponent"> Berlin </Link>
@@ -23,4 +26,6 @@ export const Header: React.StatelessComponent<{}> = () => {
                 </Toolbar>
             </AppBar>
     );
+
+
 }
